55bb93069e3f9d5c4a4480ff925b1338e178b3c8
tusmester <tusmester@gmail.com>
2018-12-11 13:19:22 +0100

    Initial commit: move source code from the main Services package.

---
+++ src/SenseNet.Messaging.MSMQ/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SenseNet.Messaging.MSMQ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sense/Net Inc.")]
+[assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
+[assembly: AssemblyProduct("SenseNet.Messaging.MSMQ")]
+[assembly: AssemblyTrademark("Sense/Net Inc.")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("7.0.0")]
+[assembly: AssemblyFileVersion("7.0.0")]
+[assembly: AssemblyInformationalVersion("7.0.0")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3f7d39ee-a737-4a07-abc0-af63aaf8a36f")]

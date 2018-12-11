93090237298c52ba8dd98723a39446c01c34ad21
tusmester <tusmester@gmail.com>
2018-12-11 14:34:46 +0100

    Add nuspec file.

---
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -0,0 +1,6 @@
+﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..\src'))
+
+# delete existing packages
+Remove-Item $PSScriptRoot\*.nupkg
+
+nuget pack $srcPath\SenseNet.Messaging.MSMQ\SenseNet.Messaging.MSMQ.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
